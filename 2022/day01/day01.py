afd208ca848116c805f0de72ef9a735d25461533
Miguel Salvá <miguelsalva@gmail.com>
2022-12-01 19:19:59 +0100

    Update day01.py

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
-  2
-  3 import sys
-  4
-  5 INPUT_FILE = "input"
-  6 ELVES_LIST = [0]
-  7 CALORIES_NUMBER = 0
-  8
-  9
- 10 def max_list(lista):
- 11   max = lista[0]
- 12   for x in lista:
- 13     if x > max:
- 14       max = x
- 15   return max
- 16
- 17
- 18 file = open(INPUT_FILE, "r")
- 19 lines = file.readlines()
- 20
- 21 for line in lines:
- 22   if line != '\n':
- 23     CALORIES_NUMBER = CALORIES_NUMBER + int(line)
- 24   else:
- 25     ELVES_LIST.append(CALORIES_NUMBER)
- 26     CALORIES_NUMBER = 0
- 27
- 28 file.close()
- 29
- 30 print(max_list(ELVES_LIST))
+
+import sys
+
+INPUT_FILE = "input"
+ELVES_LIST = [0]
+CALORIES_NUMBER = 0
+
+
+def max_list(lista):
+  max = lista[0]
+  for x in lista:
+    if x > max:
+      max = x
+  return max
+
+
+file = open(INPUT_FILE, "r")
+lines = file.readlines()
+
+for line in lines:
+  if line != '\n':
+    CALORIES_NUMBER = CALORIES_NUMBER + int(line)
+  else:
+    ELVES_LIST.append(CALORIES_NUMBER)
+    CALORIES_NUMBER = 0
+
+file.close()
+
+print(max_list(ELVES_LIST))
